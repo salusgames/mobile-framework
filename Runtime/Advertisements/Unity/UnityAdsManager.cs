f05fab27044aded02c524fe37e71fa7f4c341b2a
Dan Wilkins <dwilkins@salusgames.com>
2022-03-09 11:10:59 +0000

    Added the ability to delay between showing ads

--- Runtime/Advertisements/Unity/UnityAdsManager.cs
+++ Runtime/Advertisements/Unity/UnityAdsManager.cs
@@ -8,17 +8,20 @@ namespace SalusGames.MobileFramework.Advertisements.Unity
 {
     public class UnityAdsManager : MonoBehaviour, IUnityAdsInitializationListener
     {
-        [SerializeField] string _androidGameId;
         [SerializeField] string _iosGameId;
-        [SerializeField] bool _testMode;
-        [SerializeField] bool _enablePerPlacementMode = true;
+        [SerializeField] string _androidGameId;
         [SerializeField] private string _iosInterstitialAdId;
         [SerializeField] private string _androidInterstitialAdId;
+        [SerializeField] bool _testMode;
+        [SerializeField] bool _enablePerPlacementMode = true;
 
+        private static UnityAdsManager _instance;
         private static UnityInterstitialAd _unityInterstitialAd;
-       
+        private static bool _ignoreInterstitialAdCall;
+
         private void Awake()
         {
+            _instance = this;
             AskForTrackingIos();
             InitializeAds();
         }
@@ -51,9 +54,26 @@ namespace SalusGames.MobileFramework.Advertisements.Unity
             Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
         }
 
-        public static void ShowInterstitialAd()
+        /// <summary>
+        /// Show an Interstitial Ad
+        /// </summary>
+        /// <param name="ignoreTime">How long to ignore successive calls in seconds</param>
+        public static void ShowInterstitialAd(int ignoreTime)
         {
+            if (_ignoreInterstitialAdCall)
+            {
+                Debug.Log("Won't show an ad as not enough time has passed since last ad was shown");
+                return;
+            }
+
+            _ignoreInterstitialAdCall = true;
+            _instance.Invoke(nameof(ResetIgnoreInterstitialAdCall), ignoreTime);
             _unityInterstitialAd.ShowAd();
+        }
+
+        private void ResetIgnoreInterstitialAdCall()
+        {
+            _ignoreInterstitialAdCall = false;
         }
     }
 }
